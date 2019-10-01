27fd7a1cafb2f3084fe7bde384fa6a47b63545ca
Сэр Дъмитрий <altynd@mail.ru>
2019-10-01 23:27:53 +0400

    comm it 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+   var isfinishedNumbers = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isfinishedNumbers = true
+        
+        
     }
 
+    var numValueNew : [String] = [""]
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        if let numValue = sender.currentTitle{
+            if isfinishedNumbers{
+                displayLabel.text = numValue
+                isfinishedNumbers = false
+            } else{
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
 
 }
+//commit
 
